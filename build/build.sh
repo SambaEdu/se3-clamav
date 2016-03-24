a6458a69373701c6df90ddc66cf593911719ed2d
lolo4992 <laurentjoly@orange.fr>
2016-03-24 22:57:45 +0100

    Modification pour permettre la construction du paquet debian

---
+++ build/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+arg1="$1"
+
+script_dir=$(cd $(dirname "$0"); pwd)
+pkg_name="se3-clamav"
+
+cd "$script_dir" || {
+    echo "Error, impossible to change directory to $script_dir."
+    echo "End of the script."
+    exit 1
+}
+
+# Remove old *.deb files.
+rm -rf "$script_dir/"*.deb
+
+cp -ra "$script_dir/../sources" "$script_dir/$pkg_name"
+
+cd  "$script_dir/$pkg_name"
+find ./ \( -name *.sh -o -name *.pl -o -name *.py \) -exec chmod +x {} \;
+
+dh_clean
+debuild -uc -us -b
+# Cleaning.
+rm -r "$script_dir/$pkg_name"
+
+
